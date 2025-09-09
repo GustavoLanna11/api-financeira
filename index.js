c46b847b8ce269767e02af4ef86afbcbc80f141c
Gustavo Marques | Plano Angelus <gustavo.marques@planoangelus.com.br>
2025-09-09 08:54:07 -0300

    :sparkles: feat: adição de conexão com o banco

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/api-financeira")
 const app = express();
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+
+app.get("/", (req,res) => {
+    const user = [
+        {
+            name: "Gustavo Lanna",
+            age: 28,
+            genre: "Male",
+            bank: "Jacupiranga Banking",
+            bankAccount: 123456789 
+        },
+    ];
+    res.json(user);
+})
 
 const port = 4000;
 app.listen(port, (error)=>{
